cbb57648d612917f00bf47b3e160bbf90f80a8be
BiggiePoke <150203457+BiggiePoke@users.noreply.github.com>
2025-05-02 02:46:10 +0000

    yay

--- GetStats.hpp
+++ GetStats.hpp
@@ -6,6 +6,9 @@ struct Node {
 
 class LinkedList {
 public:
+    
+    
+
     void addHead(const int data) {
         Node* newNode = new Node;
         newNode->data = data;
@@ -14,7 +17,30 @@ public:
     }
 
     int* getStats() const{
-        //write your function here!
+        int* arr = new int[3];
+        Node* temp = head;
+        int min = temp->data;
+        int max = temp->data;
+        int mean = 0;
+        int sum = 0;
+        int numNodes = 0;
+        while (temp != nullptr){
+            if (temp->data < min){
+                min = temp->data;
+            }
+            if (temp->data > max){
+                max = temp->data;
+            }
+            sum += temp->data;
+            numNodes++;
+            temp = temp->next;
+        }
+        mean = sum/numNodes;
+        arr[0] = min;
+        arr[1] = max;
+        arr[2] = mean;
+        return arr;
+
     }
     
 
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "GetStats.hpp"
+
+int main(){
+    LinkedList list;
+
+    list.addHead(10);
+    list.addHead(2);
+    list.addHead(3);
+    list.addHead(3);
+    list.addHead(3);
+    list.addHead(3);
+    list.addHead(3);
+    
+    int* arr = list.getStats();
+
+    for ( int i = 0 ; i < 3 ; i++){
+        std::cout << arr[i] << std::endl;
+    }
+    
+    
+    
+    return 0;
+}
